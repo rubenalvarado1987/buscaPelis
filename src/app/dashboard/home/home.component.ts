4d1f582b9ea7cead72e701ff94ff9de92a76530f
Ruben Alvarado <radono@eservicios.indracompany.com>
2019-06-25 17:35:50 -0400

    funcionalidad ver detalles

--- src/app/dashboard/home/home.component.ts
+++ src/app/dashboard/home/home.component.ts
@@ -1,18 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { environment } from 'environments/environment';
 import { SettingsService } from 'app/services/settings.service'
 import { Movie } from 'app/models/movie.model';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MovieDetailComponent } from 'app/dashboard/home/movie-detail/movie-detail.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
+
 export class HomeComponent implements OnInit {
 
   movies: Movie[];
 
-  constructor(public service: SettingsService) {
+  constructor(public service: SettingsService,
+    public dialog: MatDialog) {
   }
 
   ngOnInit() {
@@ -21,7 +26,19 @@ export class HomeComponent implements OnInit {
 
   show_title(code){
     console.log('Pelicula buscada:',code);
+    this.openDialog(code)
 
+  }
+
+  openDialog(code): void {
+    const dialogRef = this.dialog.open(MovieDetailComponent, {
+      width: '800px',
+      data: {idMovie: code}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');      
+    });
   }
 
 }
